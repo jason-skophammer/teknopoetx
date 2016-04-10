49c5a96c29317f5b3968719403f2891c4d730ae7
jason-skophammer <jason.skophammer@gmail.com>
2016-04-10 00:04:31 -0400

    Update teknopoetx.py

--- teknopoetx.py
+++ teknopoetx.py
@@ -1,7 +1,10 @@
 from random import choice
-from nltk import *
+from nltk import pos_tag
+import txt_admin as admin
+import txt_formt as formt
+import re
 import json
-from pystring import S
+import abc
 
 class Settings:
 
@@ -23,35 +26,36 @@ class Settings:
         self.tmp_file = self.file_pth + 'tmp/tmp_file.txt'
         self.srce_txt = self.file_pth + self.txt_name
         self.log_file = self.file_pth + 'logs/' + self.log_fldr + '/' + self.file_idn
+        self.main_id  = self.file_pth + 'j_son/main_gram.json'
+        self.vocb_id  = self.file_pth + 'j_son/main_vocb.json'
+        self.rgrm_id  = self.file_pth + 'j_son/mrev_gram.json'
+        self.mgrm_id  = self.file_pth + 'j_son/main_gram.json'
+        self.pron_id  = self.file_pth + 'j_son/pron_dict.json'
+        self.strs_id  = self.file_pth + 'j_son/strs_dict.json'
+        self.sylc_id  = self.file_pth + 'j_son/syl_count.json'
+        self.rhym_id  = self.file_pth + 'j_son/rhym_dict.json'
 
         self.grm_strc = json.load(open(self.file_pth + 'j_son/gram_strc/pdl_grmstrc.json'))
         self.frev_grm = json.load(open(self.file_pth + 'j_son/rev_gram/' + self.rvg_name + '.json'))
 
         # Open main dictionaries used in programs
-        self.main_vocb = json.load(open(self.file_pth + 'j_son/main_vocb.json'))
+        self.main_vocb = json.load(open(self.vocb_id))
+        self.pron_dict = json.load(open(self.pron_id))
+        self.rhym_dict = json.load(open(self.rhym_id))
+        self.strs_dict = json.load(open(self.strs_id))
+        self.syl_count = json.load(open(self.sylc_id))
+        self.main_gram = json.load(open(self.mgrm_id))
+        self.mrev_gram = json.load(open(self.rgrm_id))
+
+        # Open auxillary files
+        self.freq_pron = json.load(open(self.file_pth + 'j_son/freq_pron.json'))
         self.cmu_vocab = json.load(open(self.file_pth + 'j_son/cmu_vocab.json'))
         self.brwn_vocb = json.load(open(self.file_pth + 'j_son/brwn_vocb.json'))
         self.stnd_vocb = json.load(open(self.file_pth + 'j_son/stnd_vocb.json'))
-        self.pron_dict = json.load(open(self.file_pth + 'j_son/pron_dict.json'))
-        self.freq_pron = json.load(open(self.file_pth + 'j_son/freq_pron.json'))
-        self.rhym_dict = json.load(open(self.file_pth + 'j_son/rhym_dict.json'))
-        self.strs_dict = json.load(open(self.file_pth + 'j_son/strs_dict.json'))
-        self.syl_count = json.load(open(self.file_pth + 'j_son/syl_count.json'))
-        self.main_gram = json.load(open(self.file_pth + 'j_son/main_gram.json'))
-        self.mrev_gram = json.load(open(self.file_pth + 'j_son/mrev_gram.json'))
         self.brwn_dict = json.load(open(self.file_pth + 'j_son/brwn_dict.json'))
         self.cmu_pdict = json.load(open(self.file_pth + 'j_son/cmu_pdict.json'))
         self.eng_words = json.load(open(self.file_pth + 'j_son/eng_words.json'))
         self.stp_words = json.load(open(self.file_pth + 'j_son/stp_words.json'))
-
-        self.main_file = self.main_gram
-        self.vocb_file = self.main_vocb
-        self.rgrm_file = self.mrev_gram
-        self.mgrm_file = self.main_gram
-        self.pron_file = self.pron_dict
-        self.strs_file = self.strs_dict
-        self.sylc_file = self.syl_count
-        self.rhym_file = self.rhym_dict
 
 
 class Anlyz:
@@ -307,26 +311,24 @@ class Anlyz:
         return num_syls
 
 
-    def gen_pron(self, word_list):
+    def gen_pron(self, word):
         prondict  = self.sett.cmu_pdict
         pron_dict = self.sett.pron_dict
         freq_pron = self.sett.freq_pron
-        prons = []
-        for word in word_list:
-            if word not in prondict.keys():
-                print(word)
-                word_pron = []
-                phon_cl = self.phon_clus(word)
-                for i in range(len(phon_cl)):
-                    if phon_cl[i].lower() in freq_pron.keys():
-                        phon = choice(freq_pron[phon_cl[i].lower()])
-                    else:
-                        phon = phon_cl[i].upper()
-                    word_pron.append(phon)
-                pron_dict[word] = word_pron
-                prons += word_pron
-        with open(self.sett.file_pth + 'j_son/pron_dict.json', 'w') as mpd: json.dump(pron_dict, mpd)
-        return prons
+        if word not in prondict.keys():
+            print(word)
+            word_pron = []
+            phon_cl = self.phon_clus(word)
+            for i in range(len(phon_cl)):
+                if phon_cl[i].lower() in freq_pron.keys():
+                    phon = choice(freq_pron[phon_cl[i].lower()])
+                else:
+                    phon = phon_cl[i].upper()
+                word_pron.append(phon)
+            pron_dict[word] = word_pron
+            json.dump(pron_dict, open(self.sett.file_pth + self.sett.pron_id, 'w'))
+        else: word_pron = prondict[word]
+        return word_pron
 
 
     def gram_struc(self, wordlist):
@@ -346,39 +348,369 @@ class Anlyz:
                     mrev_gram[pos] = [word]
         return sent_pos
 
+class File:
+
+    __metaclass__ = abc.ABCMeta
+
+    def __init__(self, f_id='', add=None):
+        self.sett = Settings()
+        self.anlz = Anlyz()
+        self.add  = add
+        self.fnct = (lambda x: x == x)
+        self.updt = self.sett.main_gram
+        self.fltr = self.updt.keys()
+        self.m_id = self.sett.main_id
+        self.f_id = 'j_son/' + f_id + '.json'
+        self.f_ex = self.sett.file_pth + self.f_id
+
+    def generate(self, data):
+        gen = self.process(data)
+        self.save_data(gen, self.f_ex)
+        return gen
+
+    def update(self):
+        main_file = self.updt
+        new = [w for w in self.add if w not in self.fltr and self.updt[w]]
+        print(len(new))
+        self.enter(main_file, self.add)
+        self.save_data(main_file, self.m_id)
+        print('update complete')
+
+    def process(self, data):
+        words = list(set(formt.remove_punct(data)))
+        new_dict = {}
+        self.enter(new_dict, words)
+        return new_dict
+
+    def enter(self, main_file, data):
+        for x in data:
+            if x in self.fltr and self.updt[x]:
+                main_file[x] = self.updt[x]
+            else:
+                print(x)
+                main_file[x] = self.fnct(x)
+
+    @staticmethod
+    def save_data(data, ext):
+        json.dump(data, open(ext, 'w'))
+
+
+class Vocab(File):
+
+    def __init__(self, f_id='', add=None):
+        super(Vocab, self).__init__(f_id='', add=None)
+        self.add  = add
+        self.updt = self.sett.main_vocb
+        self.fltr = self.sett.main_vocb
+        self.m_id = self.sett.vocb_id
+        self.f_id = 'j_son/vocab/' + f_id + '_vocb.json'
+        self.f_ex = self.sett.file_pth + self.f_id
+
+    def process(self, file):
+        words = admin.word_list(file)
+        voc = list(set(formt.remove_punct(words)))
+        vocab = list(set([w for w in voc if w.isalpha()]))
+        vocab += formt.lower_case(vocab)
+        vocab = list(set(vocab))
+        for x in [w for w in vocab if re.findall(r'^[ivxlcIVXLC]+$', w)
+        and w not in ['I', 'i', 'Ill', 'ill', 'civil', 'Civil']]:
+            vocab.remove(x)
+        return vocab
+
+    def enter(self, main_file, word_list):
+        for word in word_list:
+            print(word)
+            main_file.append(word)
+        return main_file
+
+
+class MainGram(File):
+
+    def __init__(self, f_id='', add=None):
+        super(MainGram, self).__init__(f_id='', add=None)
+        self.add  = add
+        self.updt = self.sett.main_gram
+        self.fltr = self.updt.keys()
+        self.m_id = self.sett.mgrm_id
+        self.f_id = 'j_son/main_grm/' + f_id + '_mg.json'
+        self.f_ex = self.sett.file_pth + self.f_id
+
+    def enter(self, main_file, word_list):
+        for word in word_list:
+            if word in self.sett.brwn_dict.keys():
+                pos = self.sett.brwn_dict[word]
+            else: pos = pos_tag([word])[0][1]
+            print(word)
+            main_file[word] = pos
+        return main_file
+
+
+class RevGram(File):
+
+    def __init__(self, f_id='', add=None):
+        super(RevGram, self).__init__(f_id='', add=None)
+        self.add  = add
+        self.updt = self.sett.mrev_gram
+        self.fltr = self.sett.main_gram.keys()
+        self.m_id = self.sett.rgrm_id
+        self.f_id = 'j_son/rev_gram/' + f_id + '_rg.json'
+        self.f_ex = self.sett.file_pth + self.f_id
+
+    def process(self, word_list):
+        words = list(set(formt.remove_punct(word_list)))
+        old_words = [w for w in words if w in self.sett.main_gram.keys()]
+        new_words = [w for w in words if w not in old_words]
+        rev_gram = {}
+        for pos in self.sett.main_gram.values():
+            rev_gram.setdefault(pos, [])
+        pairs = pos_tag(new_words)
+        gram_list = list(set([y for x, y in pairs]))
+        for pos in gram_list:
+            rev_gram[pos] = list(set([pairs[i][0] for i in range(len(new_words)) if pairs[i][1] == pos]))
+        for word in old_words:
+            rev_gram[self.sett.main_gram[word]].append(word)
+        return rev_gram
+
+    def enter(self, main_file, word_list):
+        for word in word_list:
+            if word in self.sett.brwn_dict.keys():
+                pos = self.sett.brwn_dict[word]
+            else:
+                pos = pos_tag([word])[0][1]
+                print(word)
+            if pos in main_file.keys():
+                main_file[pos].append(word)
+            else: main_file[pos] = [word]
+        return main_file
+
+
+class StrsDict(File):
+
+    def __init__(self, f_id='', add=None):
+        super(StrsDict, self).__init__(f_id='', add=None)
+        self.add  = add
+        self.updt = self.sett.strs_dict
+        self.m_id = self.sett.strs_id
+        self.f_id = 'j_son/strs_dct/' + f_id + '_strs.json'
+        self.f_ex = self.sett.file_pth + self.f_id
+
+    def enter(self, main_file, word_list):
+        for word in word_list:
+            print(word)
+            main_file[word] = self.anlz.strs_pattrn(word)
+        admin.fix_strs_dict(word_list, main_file)
+        return main_file
+
+
+class PronDict(File):
+
+    def __init__(self, f_id='', add=None):
+        super(PronDict, self).__init__(f_id='', add=None)
+        self.add  = add
+        self.updt = self.sett.pron_dict
+        self.fltr = self.updt.keys()
+        self.fnct = (lambda x: self.anlz.gen_pron(x))
+        self.m_id = self.sett.pron_id
+        self.f_id = 'j_son/pron_dct/' + f_id + '_prndct.json'
+        self.f_ex = self.sett.file_pth + self.f_id
+
+
+class SylDict(File):
+
+    def __init__(self, f_id='', add=None):
+        super(SylDict, self).__init__(f_id='', add=None)
+        self.add  = add
+        self.updt = self.sett.syl_count
+        self.fnct = (lambda x: self.anlz.count_syl(x))
+        self.m_id = self.sett.sylc_id
+        self.f_id = 'j_son/syl_cnt/' + f_id + '_sylc.json'
+        self.f_ex = self.sett.file_pth + self.f_id
+
+
+class RhymDict(File):
+
+    def __init__(self, f_id='', add=None):
+        super(RhymDict, self).__init__(f_id='', add=None)
+        self.add  = add
+        self.updt = self.sett.rhym_dict
+        self.fnct = (lambda x: self.anlz.last_syl(x))
+        self.m_id = self.sett.rhym_id
+        self.f_id = 'j_son/rhym_dct/' + f_id + '_rhym.json'
+        self.f_ex = self.sett.file_pth + self.f_id
+
+class S(str):
+
+    def __init__(self, string):
+        super(S, self).__init__()
+        self.string = string
+        self.list   = list(self.string)
+        self.len    = len(self)
+
+    def __and__(self, other):
+        slice1 = [self.string[i:j] for i in range(len(self.string))
+                  for j in range(1, len(self.string) + 1) if i < j]
+        slice2 = [other.string[i:j] for i in range(len(other.string))
+                  for j in range(1, len(other.string) + 1) if i < j]
+        slice_inter = list(set([s for s in slice1 if s in slice2]))
+        max_slice = [s for s in slice_inter if len(s) == max([len(s) for s in slice_inter])]
+        return max_slice
+
+    def __lt__(self, other):
+        if isinstance(other, str):
+            return self.string in other and self.string != other
+        elif isinstance(other, S):
+            return self.string in other.string and self.string != other.string
+
+    def __gt__(self, other):
+        if isinstance(other, str):
+            return other in self.string and self.string != other
+        elif isinstance(other, S):
+            return other.string in self.string and other.string != self.string
+
+    def __le__(self, other):
+        if isinstance(other, str):
+            return self.string in other
+        elif isinstance(other, S):
+            return self.string in other.string
+
+    def __ge__(self, other):
+        if isinstance(other, str):
+            return other in self.string
+        elif isinstance(other, S):
+            return other.string in self.string
+
+    def __iadd__(self, other):
+        if isinstance(other, str):
+            self.string += other
+            return self
+        elif isinstance(other, S):
+            self.string += other.string
+            return self
+
+    def __sub__(self, other):
+        if isinstance(other, str):
+            return self.string.replace(other, '')
+        elif isinstance(other, S):
+            return self.string.replace(other.string, '')
+
+    def __isub__(self, other):
+        if isinstance(other, str):
+            self.string -= other
+            return self
+        elif isinstance(other, S):
+            self.string -= other.string
+            return self
+
+    def __mul__(self, other):
+        if isinstance(other, str):
+            return [self.string[i] + other[j] for i in range(len(self.string))
+                    for j in range(len(other))]
+        elif isinstance(other, S):
+            return [self.string[i] + other.string[j] for i in range(len(self.string))
+                    for j in range(len(other.string))]
+
+    def __rmul__(self, other):
+        if isinstance(other, str):
+            return [other[j] + self.string[i] for i in range(len(self.string))
+                    for j in range(len(other))]
+        elif isinstance(other, S):
+            return [other.string[j] + self.string[i] for i in range(len(self.string))
+                    for j in range(len(other.string))]
+
+    def __imul__(self, other):
+        if isinstance(other, str):
+            self = self.__mul__(other)
+            return self
+        elif isinstance(other, S):
+            self = self.__mul__(other.string)
+            return self
+
+    def __pow__(self, power):
+        mult = [self]
+        for _ in range(power - 1):
+            if len(mult) == 1:
+                mult = self * self
+            else:
+                mult = [self.string[i] + s for i in range(len(self.string)) for s in mult]
+        if power == 0:
+            return [self.string]
+        elif power == 1:
+            return list(self.string)
+        else:
+            return mult
+
 
 class Text:
 
     def __init__(self, filename, file_id):
+        self.vocb    = Vocab(file_id)
+        self.rvgm    = RevGram(file_id)
+        self.mgrm    = MainGram(file_id)
+        self.strs    = StrsDict(file_id)
+        self.pron    = PronDict(file_id)
+        self.sylc    = SylDict(file_id)
+        self.rhym    = RhymDict(file_id)
         self.filename = filename
         self.file_id  = file_id
         self.file_ext = '/users/jason/teknopoetx/'
 
     @property
     def vocab(self):
-        from txt_admin import update_all, gen_vocab
         try:
             f_id = self.file_ext + 'j_son/vocab/' + self.file_id + '_vocab.json'
             with open(f_id) as file:
                 text_voc = json.load(file)
             return text_voc
         except FileNotFoundError:
-            update_all(gen_vocab(self.filename, self.file_id))
+            Vocab(self.vocb.generate(self.filename)).update()
+            MainGram(self.vocb.generate(self.filename)).update()
+            RevGram(self.vocb.generate(self.filename)).update()
+            StrsDict(self.vocb.generate(self.filename)).update()
+            PronDict(self.vocb.generate(self.filename)).update()
+            SylDict(self.vocb.generate(self.filename)).update()
+            RhymDict(self.vocb.generate(self.filename)).update()
 
     @property
     def rev_gram(self):
-        from txt_admin import gen_rev_gram
         try:
             f_id = self.file_ext + 'j_son/rev_gram/' + self.file_id + '_rev_gram.json'
             with open(f_id) as f_obj_rv:
                 revgrm = json.load(f_obj_rv)
                 return revgrm
         except FileNotFoundError:
-            gen_rev_gram(self.vocab, self.file_id)
+            self.rvgm.generate(self.vocab)
 
     def formatted(self, filename, file_id, sett):
         from txt_formt import format_text
         format_text(self, filename, file_id, sett)
+
+
+class Poem(Text):
+    def __init__(self, filename, file_idn, sett):
+        super(Poem, self).__init__(filename, file_idn)
+        self.anlz     = Anlyz()
+        self.filename = filename
+        self.file_idn = file_idn
+        self.sett     = sett
+        self.poem     = open(filename).read()
+        self.meter    = self.get_meter()
+        self.line_syl = self.get_line_syl()
+        self.subject  = self.get_subject()
+
+    def get_meter(self):
+        return self.poem
+
+    def get_line_syl(self):
+        line_syls = []
+        for line in self.poem:
+            syls = 0
+            for word in line:
+                syls += self.anlz.count_syl(word)
+            line_syls.append(syls)
+        return line_syls
+
+    def get_subject(self):
+        return self.poem
 
 
 class Word(S):
@@ -433,11 +765,6 @@ class Word(S):
     def simw(self):   return self.anlz.sim_words(self.word)
     def lstv(self):   return self.anlz.last_vowel(self.word)
     def lstsyl(self): return self.anlz.last_syl(self.word)
-
-
-
-
-
 
 
 
